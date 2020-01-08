26a27d768f862055340e0b429bd5ba75042006f2
Tom Harrop <twharrop@gmail.com>
2020-01-09 12:13:46 +1300

    upgrade racon

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='racon_chunks',
-    version='0.0.5',
+    version='0.0.6',
     description='python3 wrapper for racon_chunks',
     long_description=readme(),
     url='https://github.com/tomaharrop/racon_chunks',
